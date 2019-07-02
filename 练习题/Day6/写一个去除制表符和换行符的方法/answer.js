c11e0d8955ea86e3df303cb973bc36d9c6415abc
Konata9 <maochichen@sunmi.com>
2019-07-02 13:40:17 +0800

    1. 添加 day6 js 解答

---
+++ 练习题/Day6/写一个去除制表符和换行符的方法/answer.js
@@ -0,0 +1,12 @@
+/**
+ * 写一个去除制表符和换行符的方法
+ * 制表符 \t
+ * 换行符 \n, \r\n
+ */
+
+const removeSymbol = (str) => str.replace(/\t|\n|\r/g, "");
+
+const str =
+  "\t11122233\n_aaaaaaa\r\n_bbbbbb\t_3333333\r_4444444\n_555555";
+  
+console.log(removeSymbol(str));
