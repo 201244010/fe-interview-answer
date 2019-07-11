c7ba8d9acea0de37c483a58da397d5513945de71
Konata9 <maochichen@163.com>
2019-07-11 18:49:00 +0800

    1. 补充 Day24 js 答案

--- 练习题/Day24/如何快速让一个数组乱序，写出来/answer.js
+++ 练习题/Day24/如何快速让一个数组乱序，写出来/answer.js
@@ -8,6 +8,14 @@ const randomSort = () => (Math.random() > 0.5 ? 1 : -1);
 
 // 可以使用洗牌算法进行排序，效果会比较好
 // 洗牌算法的原理是交换数组中随机的两个位置
-const shuffle = arr => {};
+const shuffle = (arr) => {
+  const arrLen = arr.length;
+  for (let i = 0; i < arrLen; i++) {
+    const randomIndex = Math.floor(Math.random() * arrLen);
+    [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
+  }
+  return arr;
+};
 
 console.log([1, 2, 3, 4, 5].sort(randomSort));
+console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
